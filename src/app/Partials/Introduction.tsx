3b35536ce55d10b69d92ff19641e92e77187e97f
Simon Vrachliotis <simon@simonswiss.com>
2023-05-14 17:48:36 +1000

    initial commit

---
+++ src/app/Partials/Introduction.tsx
@@ -0,0 +1,176 @@
+import {
+    CloudArrowUpIcon,
+    LockClosedIcon,
+    ServerIcon,
+} from "@heroicons/react/20/solid";
+
+export default function Introduction() {
+    return (
+        <>
+            <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+                <div className="mx-auto max-w-2xl text-center">
+                    <p className="text-base font-semibold leading-7 text-indigo-600">
+                        Your journey starts today
+                    </p>
+                    <h2 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                        Let's run together!
+                    </h2>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                        We are passionate about teaching children and adults a
+                        love of running through a dynamic and unique form of
+                        training. As running is the foundation of most sports,
+                        we strive to bring out the best in each individual.
+                    </p>
+                </div>
+            </div>
+
+            <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+                <div className="absolute inset-0 -z-10 overflow-hidden">
+                    <svg
+                        className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
+                        aria-hidden="true"
+                    >
+                        <defs>
+                            <pattern
+                                id="e813992c-7d03-4cc4-a2bd-151760b470a0"
+                                width={200}
+                                height={200}
+                                x="50%"
+                                y={-1}
+                                patternUnits="userSpaceOnUse"
+                            >
+                                <path d="M100 200V.5M.5 .5H200" fill="none" />
+                            </pattern>
+                        </defs>
+                        <svg
+                            x="50%"
+                            y={-1}
+                            className="overflow-visible fill-gray-50"
+                        >
+                            <path
+                                d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+                                strokeWidth={0}
+                            />
+                        </svg>
+                        <rect
+                            width="100%"
+                            height="100%"
+                            strokeWidth={0}
+                            fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)"
+                        />
+                    </svg>
+                </div>
+                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+                    <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                        <div className="lg:pr-4">
+                            <div className="lg:max-w-lg">
+                                <p className="text-base font-semibold leading-7 text-indigo-600">
+                                    It's more than running.
+                                </p>
+                                <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                                    A way of life.
+                                </h1>
+                                <p className="mt-6 text-xl leading-8 text-gray-700">
+                                    Beyond improving performance, we also build
+                                    confidence, focus and concentration. We
+                                    firmly believe that a strong mind and fit
+                                    body are key components to success and
+                                    happiness in life.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+                        <img
+                            className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+                            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                            alt=""
+                        />
+                    </div>
+                    <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+                        <div className="lg:pr-4">
+                            <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
+                                <p>
+                                    Our classes for children are aimed at
+                                    developing good technique and a life-long
+                                    desire to run and stay fit. By planting the
+                                    seeds for success through individualised
+                                    goal-setting, children learn about the
+                                    pleasure of setting and achieving goals –
+                                    all within a safe, monitored framework.
+                                </p>
+                                {/* <ul
+                                    role="list"
+                                    className="mt-8 space-y-8 text-gray-600"
+                                >
+                                    <li className="flex gap-x-3">
+                                        <CloudArrowUpIcon
+                                            className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                            aria-hidden="true"
+                                        />
+                                        <span>
+                                            <strong className="font-semibold text-gray-900">
+                                                Push to deploy.
+                                            </strong>{" "}
+                                            Lorem ipsum, dolor sit amet
+                                            consectetur adipisicing elit.
+                                            Maiores impedit perferendis suscipit
+                                            eaque, iste dolor cupiditate
+                                            blanditiis ratione.
+                                        </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <LockClosedIcon
+                                            className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                            aria-hidden="true"
+                                        />
+                                        <span>
+                                            <strong className="font-semibold text-gray-900">
+                                                SSL certificates.
+                                            </strong>{" "}
+                                            Anim aute id magna aliqua ad ad non
+                                            deserunt sunt. Qui irure qui lorem
+                                            cupidatat commodo.
+                                        </span>
+                                    </li>
+                                    <li className="flex gap-x-3">
+                                        <ServerIcon
+                                            className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                                            aria-hidden="true"
+                                        />
+                                        <span>
+                                            <strong className="font-semibold text-gray-900">
+                                                Database backups.
+                                            </strong>{" "}
+                                            Ac tincidunt sapien vehicula erat
+                                            auctor pellentesque rhoncus. Et
+                                            magna sit morbi lobortis.
+                                        </span>
+                                    </li>
+                                </ul> */}
+                                <p className="mt-8">
+                                    Teenagers, whose bodies and brains are going
+                                    through tremendous physical and chemical
+                                    changes throughout adolescence, benefit from
+                                    physical challenges that either sharpen
+                                    their existing sporting skills or establish
+                                    the fitness foundation that they may not yet
+                                    have developed.
+                                </p>{" "}
+                                <p className="mt-8">
+                                    Achieving personal bests in a social
+                                    environment outside their usual circle of
+                                    friends and school peers is very motivating.
+                                </p>
+                                <p className="mt-8">
+                                    Come join the fun - we're looking forward to
+                                    meeting you!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,112 @@
+'use client'
+
+import { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+import Logo from '@/components/Logo'
+
+const navigation = [
+  { name: 'Sydney', href: '#' },
+  { name: 'Woolgoolga', href: '#' },
+  // { name: "Marketplace", href: "#" },
+  // { name: "Company", href: "#" },
+]
+
+export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <div className="bg-white">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav
+          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
+          <div className="flex lg:flex-1">
+            <Link href="#" className="group">
+              <span className="sr-only">Star Athletics</span>
+              <Logo className="h-9 group-hover:-rotate-1 transition" />
+            </Link>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <Link href={'#'} className="text-sm font-semibold leading-6 text-gray-900">
+              Dashboard <span aria-hidden="true">&rarr;</span>
+            </Link>
+          </div>
+        </nav>
+        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+          <div className="fixed inset-0 z-50" />
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <Link href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <Image
+                  width={32}
+                  height={32}
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt=""
+                />
+              </Link>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
+                </div>
+                <div className="py-6">
+                  <Link
+                    href={'#'}
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Dashboard
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </Dialog.Panel>
+        </Dialog>
+      </header>
+    </div>
+  )
+}
