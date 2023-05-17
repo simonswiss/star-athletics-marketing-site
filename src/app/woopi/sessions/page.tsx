ceda28d37eb50f905f603d502e88ca95f5ff3ca2
Simon Vrachliotis <simon@simonswiss.com>
2023-05-17 15:49:10 +1000

    session capacity and spots left

--- src/app/woopi/sessions/page.tsx
+++ src/app/woopi/sessions/page.tsx
@@ -1,47 +1,79 @@
-import { InboxIcon, TrashIcon, UsersIcon } from '@heroicons/react/24/outline'
+import clsx from 'clsx'
+import { MapIcon, ClockIcon } from '@heroicons/react/24/outline'
 
-const features = [
+type Session = {
+  name: string
+  slug: string
+  day: 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday'
+  startTime: string
+  endTime: string
+  capacity: number
+  availability: number
+  description: string
+  location: string
+  images?: string[]
+}
+
+const sessions: Session[] = [
   {
     name: 'Speed & Agility',
+    slug: 'speed-and-agility',
     description:
-      'Non quo aperiam repellendus quas est est. Eos aut dolore aut ut sit nesciunt. Ex tempora quia. Sit nobis consequatur dolores incidunt.',
-    href: '#',
-    icon: InboxIcon,
+      'A session focused on speed and agility. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam necessitatibus incidunt ut officiis explicabo inventore.',
+    day: 'Monday',
+    startTime: '4:00pm',
+    endTime: '5:00pm',
+    capacity: 20,
+    availability: 7,
+    location: 'Location goes here',
   },
   {
-    name: 'Endurance Training',
+    name: 'Core Strength',
+    slug: 'core-strength',
     description:
-      'Vero eum voluptatem aliquid nostrum voluptatem. Vitae esse natus. Earum nihil deserunt eos quasi cupiditate. A inventore et molestiae natus.',
-    href: '#',
-    icon: UsersIcon,
+      'A session focused on core strength. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam necessitatibus incidunt ut officiis explicabo inventore.',
+    day: 'Tuesday',
+    startTime: '7:00am',
+    endTime: '8:00am',
+    capacity: 12,
+    availability: 12,
+    location: 'Location goes here',
   },
   {
-    name: 'Boot Camp',
+    name: 'Sprints Clinic',
+    slug: 'sprints-clinic',
     description:
-      'Et quod quaerat dolorem quaerat architecto aliquam accusantium. Ex adipisci et doloremque autem quia quam. Quis eos molestiae at iure impedit.',
-    href: '#',
-    icon: TrashIcon,
+      'A session focused on top speed. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam necessitatibus incidunt ut officiis explicabo inventore.',
+    day: 'Tuesday',
+    startTime: '6:00pm',
+    endTime: '7:00pm',
+    capacity: 25,
+    availability: 2,
+    location: 'Location goes here',
   },
   {
-    name: 'Speed & Agility',
+    name: 'Long Distance',
+    slug: 'long-distance',
     description:
-      'Non quo aperiam repellendus quas est est. Eos aut dolore aut ut sit nesciunt. Ex tempora quia. Sit nobis consequatur dolores incidunt.',
-    href: '#',
-    icon: InboxIcon,
+      'A session focused on long distance. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam necessitatibus incidunt ut officiis explicabo inventore.',
+    day: 'Wednesday',
+    startTime: '7:00am',
+    endTime: '8:00am',
+    capacity: 30,
+    availability: 21,
+    location: 'Location goes here',
   },
   {
-    name: 'Endurance Training',
+    name: 'Ultra Trail Prep',
+    slug: 'ultra-trail-prep',
     description:
-      'Vero eum voluptatem aliquid nostrum voluptatem. Vitae esse natus. Earum nihil deserunt eos quasi cupiditate. A inventore et molestiae natus.',
-    href: '#',
-    icon: UsersIcon,
-  },
-  {
-    name: 'Boot Camp',
-    description:
-      'Et quod quaerat dolorem quaerat architecto aliquam accusantium. Ex adipisci et doloremque autem quia quam. Quis eos molestiae at iure impedit.',
-    href: '#',
-    icon: TrashIcon,
+      'A session focused on those preparing for the Ultra 100. Yes, some people willingly do that! Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    day: 'Thursday',
+    startTime: '6:00pm',
+    endTime: '7:00pm',
+    capacity: 10,
+    availability: 6,
+    location: 'Location goes here',
   },
 ]
 
@@ -61,27 +93,58 @@ export default function Example() {
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
-            {features.map((feature) => (
-              <div key={feature.name} className="flex flex-col">
-                <dt className="text-base font-semibold leading-7 text-gray-900">
-                  <div className="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-purple-600">
-                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
-                  </div>
-                  {feature.name}
-                </dt>
-                <dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600">
-                  <p className="flex-auto">{feature.description}</p>
-                  <p className="mt-6">
-                    <a
-                      href={feature.href}
-                      className="text-sm font-semibold leading-6 text-purple-600"
-                    >
-                      Learn more <span aria-hidden="true">→</span>
-                    </a>
-                  </p>
-                </dd>
-              </div>
-            ))}
+            {sessions.map((session) => {
+              const spotsLeft = session.capacity - session.availability
+              let availabilityClasses = 'bg-green-100 text-green-600'
+              if (spotsLeft <= 5) {
+                availabilityClasses = 'bg-yellow-100 text-yellow-600'
+              }
+              if (spotsLeft === 0) {
+                availabilityClasses = 'bg-red-100 text-red-600'
+              }
+
+              return (
+                <div key={session.name} className="flex flex-col">
+                  <dt className="text-base font-semibold leading-7 text-gray-900">
+                    {session.name}
+                  </dt>
+                  <dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                    <div className="mt-2 flex items-center gap-2">
+                      <ClockIcon className="h-5 w-5 text-slate-400" />
+                      <p className="text-sm text-slate-500">
+                        {session.day}, {session.startTime} – {session.endTime}
+                      </p>
+                    </div>
+                    <div className="mt-2 flex items-center gap-2">
+                      <MapIcon className="h-5 w-5 text-slate-400" />
+                      <p className="text-sm text-slate-500">{session.location}</p>
+                    </div>
+                    <div className="mt-4">
+                      <span
+                        className={clsx(
+                          'inline-flex items-center gap-1 self-start rounded-full px-2 py-1 text-xs font-medium',
+                          availabilityClasses
+                        )}
+                      >
+                        <span className="font-semibold">
+                          {spotsLeft ? `${spotsLeft} spots left` : 'Session Full'}
+                        </span>{' '}
+                        <span>({session.capacity} total)</span>
+                      </span>
+                    </div>
+                    <p className="mt-4 flex-auto">{session.description}</p>
+                    <p className="mt-6">
+                      <a
+                        href={session.slug}
+                        className="text-sm font-semibold leading-6 text-purple-600"
+                      >
+                        Learn more <span aria-hidden="true">→</span>
+                      </a>
+                    </p>
+                  </dd>
+                </div>
+              )
+            })}
           </dl>
         </div>
       </div>
