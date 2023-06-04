f777061874ae142d76c1dc029a9c5600c1233ec3
Simon Vrachliotis <simon@simonswiss.com>
2023-06-04 17:46:55 +1000

    coaches page

--- src/app/(app)/coaches/page.tsx
+++ src/app/(app)/coaches/page.tsx
@@ -1,57 +1,54 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import { twMerge } from 'tailwind-merge'
 import { DocumentRenderer } from '@keystatic/core/renderer'
 
 import { reader } from '@/app/keystatic/reader'
 
 export default async function Example() {
+  const coachesPage = await reader.singletons.coachesPage.readOrThrow()
   const coaches = await reader.collections.coaches.all({ resolveLinkedFiles: true })
   if (!coaches) throw new Error('No coaches found — please add in CMS')
+
   return (
-    <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl sm:text-center">
+    <div className="bg-white py-24 md:py-32">
+      <div className="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-5">
+        <div className="max-w-2xl xl:col-span-2">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Meet the coaches
+            {coachesPage.title}
           </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            We’re a dynamic group of individuals who are passionate about what we do and dedicated
-            to delivering the best results for our clients.
-          </p>
+          <p className="mt-6 text-lg leading-8 text-gray-600">{coachesPage.introText}</p>
         </div>
-        <ul
-          role="list"
-          className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-6 gap-y-20 sm:grid-cols-2 lg:max-w-4xl lg:gap-x-8 xl:max-w-none"
-        >
+        <ul role="list" className="-mt-12 space-y-12 divide-y divide-gray-200 xl:col-span-3">
           {coaches.map((coach) => (
-            <li key={coach.entry.name} className="flex flex-col gap-6 xl:flex-row">
+            <li key={coach.entry.name} className="flex flex-col gap-10 pt-12 sm:flex-row">
               {coach.entry.avatar && (
                 <Image
-                  width={400}
-                  height={600}
                   className="aspect-[4/5] w-52 flex-none rounded-2xl object-cover"
                   src={coach.entry.avatar}
+                  width={400}
+                  height={800}
                   alt=""
                 />
               )}
-              <div className="flex-auto">
+              <div className="max-w-xl flex-auto">
                 <h3 className="text-lg font-semibold leading-8 tracking-tight text-gray-900">
-                  {coach.entry.name}
+                  <Link className="hover:underline" href={`/coaches/${coach.slug}`}>
+                    {coach.entry.name}
+                  </Link>
                 </h3>
-                <p className="text-base text-gray-600">{coach.entry.role}</p>
-                <hr className="mt-4 w-12" />
-                <div className="mt-6 text-base leading-7 text-gray-600">
-                  <DocumentRenderer document={coach.entry.bio} />
-                </div>
-                <hr className="mt-6 w-12" />
-                <div className="mt-6">
-                  <a
-                    href={`${coach.entry.region.toLowerCase()}/sessions`}
-                    className="text-sm font-semibold leading-6 text-gray-900"
-                  >
-                    {coach.entry.name}&apos;s sessions in {coach.entry.region}{' '}
-                    <span aria-hidden="true">→</span>
-                  </a>
-                </div>
+                <p className="text-base leading-7 text-gray-600">{coach.entry.role}</p>
+                <p className="mt-6 text-base leading-7 text-gray-600">{coach.entry.shortIntro}</p>
+
+                <Link
+                  href={`/coaches/${coach.slug}`}
+                  className={twMerge(
+                    'mt-6 block text-base font-semibold leading-7 text-sky-500 hover:underline',
+                    coach.entry.region === 'Sydney' ? 'text-purple-600' : 'text-sky-500'
+                  )}
+                >
+                  Read full bio <span aria-hidden="true">→</span>
+                </Link>
               </div>
             </li>
           ))}
