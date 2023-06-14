2f9d7418b1cfd71fd01e0938fe14c8db06c12c7c
Simon Vrachliotis <simon@simonswiss.com>
2023-06-14 13:30:06 +1000

    Different cropping on coaches page

--- src/app/(app)/coaches/page.tsx
+++ src/app/(app)/coaches/page.tsx
@@ -24,12 +24,15 @@ export default async function Example() {
         </div>
         <ul role="list" className="-mt-12 space-y-12 divide-y divide-gray-200 xl:col-span-3">
           {coaches.map((coach) => (
-            <li key={coach.entry.name} className="flex flex-col gap-10 pt-12 sm:flex-row">
+            <li
+              key={coach.entry.name}
+              className="flex flex-col items-start gap-10 pt-12 sm:flex-row"
+            >
               {coach.entry.avatar && (
                 <Image
-                  className="aspect-[4/5] w-52 flex-none rounded-2xl object-cover"
+                  className="aspect-[4/5] w-52 rounded-2xl object-cover"
                   src={coach.entry.avatar}
-                  width={400}
+                  width={800}
                   height={800}
                   alt=""
                 />
