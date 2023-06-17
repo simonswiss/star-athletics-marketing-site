6df3be3f95af687e977e6fcef26950fe346e6cd8
Simon Vrachliotis <simon@simonswiss.com>
2023-06-17 13:45:07 +1000

    Hiding woopi schools, adding woopi popup clinics

--- src/app/keystatic/keystatic.config.tsx
+++ src/app/keystatic/keystatic.config.tsx
@@ -1,26 +1,31 @@
-import { config, collection, fields, component } from '@keystatic/core'
+import { config, collection } from '@keystatic/core'
 
 import { storage } from './storage'
+
 import { homepage } from './schema/homepage'
+import { coachesPage } from './schema/coaches-page'
+import { nswEventsPage } from './schema/nsw-events-page'
+import { registerPage } from './schema/register-page'
+import { contacts } from './schema/contacts'
+
 import { sydneyPage } from './schema/sydney-page'
 import { sydneySessionsPage } from './schema/sydney-sessions-page'
 import { sydneyOneOnOne } from './schema/sydney-one-on-one'
 import { sydneyHolidayCamps } from './schema/sydney-holiday-camps'
+import { sydneyPopupClinics } from './schema/sydney-popup-clinics'
+
 import { woopiPage } from './schema/woopi-page'
 import { woopiSessionsPage } from './schema/woopi-sessions-page'
 import { woopiOneOnOne } from './schema/woopi-one-on-one'
 import { woopiHolidayCamps } from './schema/woopi-holiday-camps'
-import { sydneyPopupClinics } from './schema/sydney-popup-clinics'
+import { woopiPopupClinics } from './schema/woopi-popup-clinics'
 import { woopiSchools } from './schema/woopi-schools'
-import { coachesPage } from './schema/coaches-page'
-import { sessionSchema } from './schema/sessions'
+
 import { coaches } from './schema/coaches'
+import { nswEvents } from './schema/nsw-events'
+import { sessionSchema } from './schema/sessions'
 import { testimonials } from './schema/testimonials'
 import { faqs } from './schema/faqs'
-import { nswEventsPage } from './schema/nsw-events-page'
-import { nswEvents } from './schema/nsw-events'
-import { contacts } from './schema/contacts'
-import { registerPage } from './schema/register-page'
 
 export default config({
   storage,
@@ -38,6 +43,7 @@ export default config({
     woopiSessionsPage,
     woopiOneOnOne,
     woopiHolidayCamps,
+    woopiPopupClinics,
     nswEventsPage,
     contacts,
     registerPage,
