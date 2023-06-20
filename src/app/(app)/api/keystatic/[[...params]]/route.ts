e36b1d816043e4c703324b8b348c8e097aebf43a
Simon Vrachliotis <simon@simonswiss.com>
2023-06-20 12:03:24 +1000

    comment out bodyParser size limit and response limit

--- src/app/(app)/api/keystatic/[[...params]]/route.ts
+++ src/app/(app)/api/keystatic/[[...params]]/route.ts
@@ -1,14 +1,14 @@
 import { makeRouteHandler } from '@keystatic/next/route-handler'
 import keystaticConfig from '@/app/keystatic/keystatic.config'
 
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: '20mb',
-    },
-    responseLimit: '20mb',
-  },
-}
+// export const config = {
+//   api: {
+//     bodyParser: {
+//       sizeLimit: '20mb',
+//     },
+//     responseLimit: '20mb',
+//   },
+// }
 
 export const { POST, GET } = makeRouteHandler({
   config: keystaticConfig,
